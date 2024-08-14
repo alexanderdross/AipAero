ba392c4951b7d9b8c843ec14f9c33b85cf2f9548
Nicolas Zedler <nicolas.zedler@deltasecure.de>
2024-08-14 19:00:48 +0200

    Add trailing slashes

--- next.config.js
+++ next.config.js
@@ -8,6 +8,8 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  trailingSlash: true,
+};
 
 export default withNextIntl(config);
