5e14837d5a91c15b58a37b515cf787d21be79988
Nicolas Zedler <nicolas.zedler@deltasecure.de>
2024-09-02 14:04:28 +0200

    Locale switcher dropdown

--- src/app/_components/locale-switcher.tsx
+++ src/app/_components/locale-switcher.tsx
@@ -1,19 +1,23 @@
 "use client";
 
-import { useLocale } from 'next-intl';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import { useLocale, useTranslations } from 'next-intl';
+import { usePathname, useRouter } from 'next/navigation';
 
 export function LocaleSwitcher() {
+  const router = useRouter();
   const pathname = usePathname();
   const locale = useLocale();
-  const currentLocale = pathname.includes('/en/') ? 'en' : locale;
-  const nextLocale = currentLocale === 'en' ? locale : 'en';
-  const nextLocaleHref = currentLocale === 'en' ? ".." : 'en/';
+  const t = useTranslations('LocaleSwitcher');
+  const currentLocaleSlug = pathname.includes('/en/') ? 'en' : locale;
+  const currentLocale = currentLocaleSlug === 'en' ? t('english') : t('native');
+  const nextLocale = currentLocaleSlug === 'en' ? t('native') : t('english');
+  const nextLocaleHref = currentLocaleSlug === 'en' ? ".." : 'en/';
   return (
     <>
-      <p>Current Locale {currentLocale} </p>
-      <Link href={nextLocaleHref} title={`switch to ${nextLocale}`} className='text-drossblue hover:underline'>Next Locale {nextLocale} </Link>
+      <select title="switch language" onChange={() => router.push(nextLocaleHref)}>
+        <option value={currentLocale} title={`switch to ${currentLocale}`} rel="noopener">{currentLocale}</option>
+        <option value={nextLocale} title={`switch to ${nextLocale}`} rel="noopener">{nextLocale}</option>
+      </select>
     </>
   );
 }
