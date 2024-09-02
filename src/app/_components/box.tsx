1b0ce1bbc9be3e8c4117cba7a89f8d8ad91625e0
Nicolas Zedler <nicolas.zedler@deltasecure.de>
2024-09-02 14:35:19 +0200

    Locale switcher dropdown

--- src/app/_components/box.tsx
+++ src/app/_components/box.tsx
@@ -17,7 +17,7 @@ export function Box({
     <div className="w-full flex items-center justify-between p-6 space-x-6">
       <div className="flex-1">
         <div className="flex items-center space-x-3">
-          <h3 className="text-gray-900 text-sm font-medium truncate">{title}</h3>
+          <h2 className="text-gray-900 text-sm font-medium truncate">{title}</h2>
           <span className="flex-shrink-0 inline-block px-2 py-0.5 text-green-800 text-xs font-medium bg-green-100 rounded-full">
             New
           </span>
