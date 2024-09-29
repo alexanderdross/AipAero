91dc717523d0d24735d81da900eedf2dd7ec4622
Nicolas Zedler <nicolas@zedler.xyz>
2024-09-29 09:31:23 +0200

    Make header smaller mobile

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -2,8 +2,8 @@ export function Header({ title, description }: { title: string; description: str
   return (
     <header className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div className="text-center py-[40px]">
-          <h1 className="m-0 text-[36px] break-words hyphens-auto">{title}</h1>
-          <p className="m-0 text-[18px] break-words hyphens-auto">{description}</p>
+          <h1 className="m-0 text-xl md:text-2xl lg:text-3xl break-words hyphens-auto">{title}</h1>
+          <p className="m-0 text-md md:text-xl break-words hyphens-auto">{description}</p>
       </div>
     </header>
   );
