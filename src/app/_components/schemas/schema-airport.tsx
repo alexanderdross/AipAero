c863573495818adab14e89dea647dd0d66958558
Nicolas Zedler <nicolas@zedler.xyz>
2024-10-18 21:52:37 +0200

    Fix airport schema url

--- src/app/_components/schemas/schema-airport.tsx
+++ src/app/_components/schemas/schema-airport.tsx
@@ -1,3 +1,8 @@
+'use client';
+
+import { usePathname, useSearchParams } from "next/navigation";
+import { orgUrl } from "~/app/_components/metadata";
+
 interface Props {
   name: string;
   icaoCode: string;
@@ -11,11 +16,16 @@ export function SchemaAirport({
   alternateName,
   description
 }: Props) {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const icao = Array.from(searchParams.keys()).length === 1 ? `?${Array.from(searchParams.keys())[0]}` : '';
+
   const schema = {
     "@context": "https://schema.org",
     "@type": "Airport",
     "name": name,
     "icaoCode": icaoCode,
+    "url": new URL(pathname+icao, orgUrl).toString(),
     "alternateName": alternateName,
     "description": description
   };
