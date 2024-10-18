2ae47ad0f964e8eb5f3dffedce8fd11bb633e18b
Nicolas Zedler <nicolas@zedler.xyz>
2024-10-18 23:38:19 +0200

    Increase API timeout

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -4,6 +4,9 @@ import { crawl_de } from "~/lib/crawlers/crawl_de";
 import { crawl_nl } from "~/lib/crawlers/crawl_nl";
 import { crawl_uk } from "~/lib/crawlers/crawl_uk";
 
+export const maxDuration = 60;
+export const dynamic = 'force-dynamic';
+
 export async function GET(_: NextRequest) {
   const at = await crawl_at();
   const de = await crawl_de();
