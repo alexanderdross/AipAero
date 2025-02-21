fdda49f4b8c5d2bb9ffe697b25f4c2179c09dad9
Nicolas Zedler <nicolas@zedler.xyz>
2025-02-21 10:43:45 +0100

    Change orgURL to dev

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,7 +7,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const orgUrl = new URL('https://aip.aero/');
+export const orgUrl = new URL('https://dev.aip.aero/');
 export const orgLogoUrl = new URL('/aip-logo-446x319.jpg', orgUrl);
 export const orgLogoSquareUrl = new URL('/aip-logo-450x450.jpg', orgUrl);
 export const rootTitle = 'Free AIP and Approach Charts for VFR, IFR & Heliports';
