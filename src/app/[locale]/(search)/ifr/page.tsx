2fc7fd64296ae1bb7cd25d42ce833fdd0d96a01f
Nicolas Zedler <nicolas@zedler.xyz>
2025-03-01 11:29:07 +0100

    Fix UK alternate languages

--- src/app/[locale]/(search)/ifr/page.tsx
+++ src/app/[locale]/(search)/ifr/page.tsx
@@ -65,7 +65,7 @@ export async function generateMetadata({
     description: data ? `${t('resultDescription', { airport: data.title })}🗺️` : t('metaDescription'),
     alternates: {
       canonical: currentUrl,
-      languages: Object.assign({}, ...locales.map((l) => ({
+      languages: locale === 'uk' ? undefined : Object.assign({}, ...locales.map((l) => ({
         [localeLangMapping[l] as string]: new URL(getPathname({ href: '/', locale: l }), orgUrl).toString() + `${data ? `/?${data.slug}` : ''}`
       })))
     },
