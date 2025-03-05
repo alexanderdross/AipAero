c120b99bdb6ed599aaf0e307a19dfcafff0c1f6e
Nicolas Zedler <nicolas@zedler.xyz>
2025-03-05 12:41:54 +0100

    Fix UK alternate Link

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -38,7 +38,7 @@ export async function generateMetadata(
     description: t('metaDescription'),
     alternates: {
       canonical: currentUrl,
-      languages: Object.assign({}, ...locales.map((l) => ({
+      languages: locale === 'uk' ? undefined : Object.assign({}, ...locales.map((l) => ({
         [localeLangMapping[l] as string]: new URL(getPathname({ href: '/', locale: l }), orgUrl).toString()
       })))
     },
